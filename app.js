ca2cc5bb7a9c9ab0bd5dccd7cd877fcdcde6e221
beaJenius <beajenius@yandex.ru>
2022-05-31 00:18:18 +0300

    refactor: change class names by BEM rules accoding rule 2.1 in html-and-css-extended.md, replace tag selectors by BEM classes accoding rule 3.4 html-and-css.md

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById('new-task');//Add a new task.
-var addButton=document.getElementsByTagName('button')[0];//first button
+var taskInput=document.getElementById('new-goal');//Add a new task.
+var addButton=document.querySelector('.goal__btn.btn-add');//first button
 var incompleteTaskHolder=document.getElementById('incompleted-tasks');//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
 
@@ -18,32 +18,37 @@ var completedTasksHolder=document.getElementById('completed-tasks');//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement('li');
-
+    listItem.classList.add("goal")
     //input (checkbox)
-    var checkBox=document.createElement('input');//checkbx
+    var checkBox = document.createElement('input');//checkbx
+    checkBox.classList.add("goal__cb")
     //label
-    var label=document.createElement('label');//label
+    var label = document.createElement('label');//label
+    label.classList.add("goal__name")
+    label.classList.add("name-small")
     //input (text)
-    var editInput=document.createElement('input');//text
+    var editInput = document.createElement('input');//text
+    editInput.classList.add("goal__text")
+    editInput.classList.add("name-small")
     //button.edit
     var editButton=document.createElement('button');//edit button
-
+    editButton.classList.add("goal__btn")
+    editButton.classList.add("btn-edit")
     //button.delete
-    var deleteButton=document.createElement('button');//delete button
-    var deleteButtonImg=document.createElement('img');//delete button image
+    var deleteButton = document.createElement('button');//delete button
+    deleteButton.classList.add("goal__btn")
+    deleteButton.classList.add("btn-del")
+    var deleteButtonImg = document.createElement('img');//delete button image
+    deleteButtonImg.classList.add("goal__btn-img")
 
     label.innerText=taskString;
-    label.className='task-name';
 
     //Each elements, needs appending
     checkBox.type='checkbox';
     editInput.type='text';
-    editInput.className='task-val';
 
     editButton.innerText='Edit'; //innerText encodes special characters, HTML does not.
-    editButton.className='btn-edit';
 
-    deleteButton.className='btn-del';
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -82,10 +87,10 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector('label');
+    var editInput=listItem.querySelector('.goal__text');
+    var label=listItem.querySelector('.goal__name');
     var editBtn=listItem.querySelector('.btn-edit');
-    var containsClass=listItem.classList.contains('editMode');
+    var containsClass=listItem.classList.contains('edit-mode');
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +104,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle('editMode');
+    listItem.classList.toggle('edit-mode');
 };
 
 
@@ -155,9 +160,9 @@ addButton.addEventListener('click',ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log('bind list item events');
 //select ListItems children
-    var checkBox=taskListItem.querySelector('input[type=checkbox]');
-    var editButton=taskListItem.querySelector('.btn-edit');
-    var deleteButton=taskListItem.querySelector('.btn-del');
+    var checkBox=taskListItem.querySelector('.goal__cb');
+    var editButton=taskListItem.querySelector('.goal__btn.btn-edit');
+    var deleteButton=taskListItem.querySelector('.goal__btn.btn-del');
 
 
     //Bind editTask to edit button.
